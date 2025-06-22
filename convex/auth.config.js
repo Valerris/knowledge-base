39e1731db6efa543a0bf71b33fca2945b9832d21
Valerris <valerris@worldwide.io>
2025-06-22 23:50:40 +0300

    init

---
+++ convex/auth.config.js
@@ -0,0 +1,11 @@
+export default {
+  providers: [
+    {
+      // separate app
+      // domain: "https://direct-amoeba-91.clerk.accounts.dev",
+      // trullo integration
+      domain: "https://intense-frog-90.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
